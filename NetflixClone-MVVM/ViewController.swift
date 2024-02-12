fef63ec0f092337bb34dfebe1dd148c5ed2f5070
tahaturan <tahaaturann16@gmail.com>
2024-02-12 05:29:43 +0300

    Initial Commit

---
+++ NetflixClone-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone-MVVM
+//
+//  Created by Taha Turan on 12.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NetflixClone-MVVMUITests/NetflixClone_MVVMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NetflixClone_MVVMUITestsLaunchTests.swift
+//  NetflixClone-MVVMUITests
+//
+//  Created by Taha Turan on 12.02.2024.
+//
+
+import XCTest
+
+final class NetflixClone_MVVMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
