713ef0d96057352ff750f266f6e5aae1c9212ebb
tahaturan <tahaaturann16@gmail.com>
2024-03-06 06:54:19 +0300

    Movie Model is created

--- NetflixClone-MVVM/Model/MovieModel.swift
+++ NetflixClone-MVVM/Model/MovieModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+// MARK: - Movie
+struct Movie: Codable {
+    let page: Int?
+    let results: [MovieResult]?
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Result
+struct MovieResult: Codable {
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIDS: [Int]?
+    let id: Int?
+    let originalLanguage, originalTitle, overview: String?
+    let popularity: Double?
+    let posterPath, releaseDate, title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
